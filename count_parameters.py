5c509001a1b679f630d0f0952b0dba98aacfdc49
Puneet Mishra <32441618+puneat@users.noreply.github.com>
2021-01-14 01:09:57 +0530

    Delete count_parameters.py

--- count_parameters.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-from torchdiffeq import odeint_adjoint
-
-def count_parameters(model):
-    """
-    Count number of tunable parameters.
-    """
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
