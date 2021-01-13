525d91735f6e6b01367f0bf18f553cdf77373f31
Puneet Mishra <32441618+puneat@users.noreply.github.com>
2021-01-14 01:07:28 +0530

    Add files via upload

---
+++ accuracyMetrics.py
@@ -0,0 +1,22 @@
+def checkAccuracy (model, test_dataloader):
+
+    model.eval()
+    predicted=[]
+    actuals=[]
+    for i, (inputs, targets) in tqdm.tqdm(
+    	enumerate(test_dl), total = len(test_dl), leave=False):
+        with torch.no_grad():
+            logits = model(inputs.float())
+            logits = logits.cpu()
+            inputs=inputs.cpu()
+            targets=targets.cpu()
+        preds = torch.argmax(F.softmax(logits, dim=1), axis=1).numpy()
+        targets = targets.numpy()
+        actuals = np.concatenate((actuals,targets))
+        predicted = np.concatenate((predicted,preds))
+
+    acc = (predicted == actuals).mean()
+	print(f"\n ODENet accuracy: {acc}")
+	print(f"\n  Number of tunable parameters: {count_parameters(odenet)}")
+
+
---
+++ trainingFunctions.py
@@ -0,0 +1,81 @@
+# Helpers adapted from https://pytorch.org/tutorials/beginner/nn_tutorial.html
+
+def get_model(is_odenet=True, dim=64, adam=False, **kwargs):
+    """
+    Initialize ResNet or ODENet with optimizer.
+    """
+    downsampling_layers = [
+        nn.Conv1d(1, dim, 3, 1),
+        norm(dim),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(dim, dim, 4, 2, 1),
+        norm(dim),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(dim, dim, 4, 2, 1)
+    ]
+
+    feature_layers = [ODENet(ODEfunc(dim), **kwargs)] if is_odenet else [ResBlock(dim) for _ in range(6)]
+
+    fc_layers = [norm(dim), nn.ReLU(inplace=True), nn.AdaptiveAvgPool1d(1), Flatten(), nn.Linear(dim,2)]
+
+    model = nn.Sequential(*downsampling_layers, *feature_layers, *fc_layers)
+
+    opt = optim.Adam(model.parameters()) if adam else optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+
+    return model, opt
+
+
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    """
+    Calculate loss and update weights if training.
+    """
+    loss = loss_func(model(xb.float()), yb.long())
+
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+    return loss.item(), len(xb)
+
+
+def fitModel(epochs, model, loss_func, opt, train_dl, valid_dl):
+    """
+    Train neural network model.
+    """
+    num_batches = len(train_dl)
+    best_loss=1000
+    
+    for epoch in range(epochs):
+        print(f"Training... epoch {epoch + 1}")
+        
+        model.train()   # Set model to training mode
+        batch_count = 0
+        #start = time.time()
+        for rand,(xb, yb) in tqdm.tqdm(enumerate(train_dl), total = len(train_dl), leave=False):
+            xb = xb.cuda()
+            yb = yb.cuda()
+            batch_count += 1
+            # curr_time = time.time()
+            # percent = round(batch_count/len(train_dl) * 100, 1)
+            # elapsed = round((curr_time - start)/60, 1)
+            # print(f"    Percent trained: {percent}%  Time elapsed: {elapsed} min", end='\r')
+            loss_batch(model, loss_func, xb, yb, opt)
+            
+        model.eval()    # Set model to validation mode
+        with torch.no_grad():
+            losses, nums = zip(
+                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
+            )
+        val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
+        if val_loss < best_loss:
+            best_loss = val_loss
+            path = '/gdrive/My Drive/Spectrum_Sensing/SS_NODE_PyTorch_'+str(round(best_loss,4))+'.pt'
+            torch.save(odenet, path)
+            print('\n    best model saved')
+            
+        #writer.add_scalar("Loss/train", val_loss, epoch)
+        #writer.flush()
+
+        print(f"\n    val loss: {round(val_loss, 4)}\n")
+        print(f"\n    best loss: {round(best_loss, 4)}\n")
