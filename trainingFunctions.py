5eeaea92a3a93a53fd1faab5708398b27ddae3c5
Puneet Mishra <32441618+puneat@users.noreply.github.com>
2021-01-17 22:43:44 +0530

    Update trainingFunctions.py

--- trainingFunctions.py
+++ trainingFunctions.py
@@ -17,21 +17,21 @@ from torch.utils.data import TensorDataset, DataLoader
 import tqdm
 from ModelBlocks import ConcatConv1d, ODENet, ODEfunc, ResBlock, count_parameters, norm, Flatten
 
-def get_model(is_odenet=True, dim=1, adam=False, **kwargs):
+def get_model(is_odenet=True, dim=64, adam=False, **kwargs):
     """
     Initialize ResNet or ODENet with optimizer.
     """
     downsampling_layers = [
-#         nn.Conv1d(1, dim, 1, 1) 
-#         norm(dim),
-#         nn.ReLU(inplace=True),
-#         nn.Conv1d(dim, dim, 4, 2, 1), 
-#         norm(dim),
-#         nn.ReLU(inplace=True),
-#         nn.Conv1d(dim, dim, 4, 2, 1),
-#         norm(dim),
-#         nn.ReLU(inplace=True),
-#         nn.Conv1d(dim, dim, 4, 2, 1)
+        nn.Conv1d(1, dim, 5, 1) 
+        norm(dim),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(dim, dim, 5, 2, 1), 
+        norm(dim),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(dim, dim, 5, 2, 1),
+        norm(dim),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(dim, dim, 5, 2, 1)
     ]
 
     feature_layers = [ODENet(ODEfunc(dim), **kwargs)] if is_odenet else [ResBlock(dim) for _ in range(6)]
